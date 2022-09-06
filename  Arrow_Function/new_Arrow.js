303df6c5f896c2ebe63597aa1bcd5d529c5b6e01
Cikie <daoanh123123@gmail.com>
2022-09-06 22:27:04 +0700

    destruc

---
+++  Arrow_Function/new_Arrow.js
@@ -0,0 +1,20 @@
+const inventory = [
+
+    {type: "machine", value: 5000},
+
+
+    {type: "machine", value: 650},
+
+
+    {type: "duck", value: 10},
+
+
+    {type: "furniture", value: 1200},
+
+
+    {type: "machine", value: 77}
+]
+const total = inventory
+    .filter((item) => item.type === 'machine')
+    .reduce((acc, curr) => acc + curr.value, 0)
+console.log(total)
