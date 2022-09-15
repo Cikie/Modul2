e1faba64053672283ac9131c68fbb001d3955c22
Cikie <daoanh123123@gmail.com>
2022-09-15 08:15:02 +0700

    Stack

--- Link_List/QuanLySinhVien/Main/Main1.ts
+++ Link_List/QuanLySinhVien/Main/Main1.ts
@@ -2,10 +2,10 @@ import {LinkedList} from "./LinkNew";
 
 let linklist = new LinkedList();
 linklist.insertFirst({name:"Ciki",score:8});
-linklist.insertFirst({name:"Ciki",score:5});
-linklist.insertFirst({name:"Ciki",score:1});
-linklist.insertLast({name:"Ciki",score:6});
-linklist.insertFirst({name:"Ciki",score:3});
+linklist.insertFirst({name:"Morg",score:5});
+linklist.insertFirst({name:"Dyland",score:1});
+linklist.insertLast({name:"Brok",score:6});
+linklist.insertFirst({name:"Tifany",score:3});
 
 console.log(linklist.showList());
 console.log(linklist.totalStudentsFail());
